3e0dbe936240ecb8e5e617c1645b933b9245ad3c
Quique231100 <130946779+Quique231100@users.noreply.github.com>
2024-02-28 22:30:21 -0600

    Create helloWorld.sh

---
+++ helloWorld/helloWorld.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Hola Mundo"
+echo "Presione Enter para continuar..."
+read
+echo "Listando Contenido"
+ls
+echo "Presione Enter para continuar..."
+read
+echo "Creando fichero Test..."
+mkdir Test
+echo "Fichero creado..."
+echo "Presione Enter para continuar..."
+read
+echo "Cambiando a fichero Test..."
+cd Test
+echo "Presione Enter para continuar..."
+read
+echo "Listando el contenido del fichero Test"
+ls
+echo "Presione Enter para continuar..."
+read
